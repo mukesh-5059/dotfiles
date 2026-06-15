2743259aa1a9b8871328aede41cd629ae7eb4d5e
mukesh-5059 <mukeshraj8e@gmail.com>
2026-06-15 16:19:54 +0530

    updated readme

--- .config/conky/grouped_processes.lua
+++ .config/conky/grouped_processes.lua
@@ -4,7 +4,7 @@
 require 'cairo'
 require 'cairo_xlib'
 
-local wallpaper_bg = "#252929"
+local wallpaper_bg = "#1C1B1F"
 
 local function hex_to_rgb(hex)
     hex = hex:gsub("#", "")
--- settings.js
+++
@@ -1,8 +0,0 @@
-window.default_settings = {
-    theme: "dark",
-    language: "en",
-    system: "Arch Linux",
-    fallback_session: "",
-    startup_user: "mukes",
-    themes: ["dark", "light", "purple"]
-};
