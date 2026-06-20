dc7c726255bf8ad8ca1eb0f62df246fdc141b9a7
mukesh-5059 <mukeshraj8e@gmail.com>
2026-06-20 11:42:20 +0530

    wayneko on startup

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -4,7 +4,10 @@
 
 # Quit running eww bar instances
 killall eww
+killall wayneko
 swaync-client -R
 swaync-client -rs
 # Start eww bar
 eww open bar 
+wayneko --layer top --width 1120 --margin-left 400 --margin-top 12
+
